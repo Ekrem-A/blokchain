673252cfdb04ddbd929b41a71784a500f095df1c
Ekrem-A <56740768+Ekrem-A@users.noreply.github.com>
2022-05-11 04:10:30 +0300

    blok
    blok içerisindeki index,timestep,data,previoushash değerleri classla belirtildi. Hash256 kütüphaneye eklendi

--- blokchain.js
+++ blokchain.js
@@ -1,1 +1,18 @@
-var ekrem= 1;
+const SHA256= require ('crypto-js/sha256');
+
+class blokchain1{
+    constructor(index,timestep,data,previoushash= ''){
+        this.index=index;
+        this.timestep=timestep;
+        this.data=data;
+        this.previoushash=previoushash;
+        this.hash='';
+        
+    }
+    Hashing(){
+        return SHA256 (this.index+this.timestep+JSON.stringify(this.data)+this.previoushash).toString;
+
+    }
+
+
+}
